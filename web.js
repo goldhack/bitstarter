41b2d0d63f1c146e7bb8c64c52a0a0439d348ab0
Henry <Henry@henry.com>
2013-07-25 15:17:00 +0000

    loool

--- web.js
+++ web.js
@@ -2,12 +2,10 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-
-
-var buff = fs.readFileSync('index.html', 'utf8');
+//var buff = fs.readFileSync('index.html', 'utf8');
  
 
-var ear = buff.toString('utf-8',0,50);
+//var ear = buff.toString('utf-8',0,50);
 
 app.get('/', function(request, response) {
     response.send(ear);
