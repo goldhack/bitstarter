12b57af34dfa91059c9060cc6d5b714fa4c024b4
Henry <Henry@henry.com>
2013-07-25 11:59:56 +0000

    still debugging

--- web.js
+++ web.js
@@ -3,12 +3,12 @@ var app = express();
 app.use(express.logger());
 
 var buffer = new Buffer(50);
-buffer = fs.readFileSync('/home/ubuntu/bitstarter/index.html');
+buffer = fs.readFileSync('index.html');
  
 
 
 app.get('/', function(request, response) {
-    response.send(buffer + buf.toString('utf8',0, buffer));
+    response.send(buffer + buffer.toString('utf8',0, buffer));
 });
 
 var port = process.env.PORT || 5000;
