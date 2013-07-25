76b495e0a77c676d62098316c3ab4a1e1c473ea6
Henry <Henry@henry.com>
2013-07-25 15:14:27 +0000

    loool

--- web.js
+++ web.js
@@ -4,16 +4,17 @@ app.use(express.logger());
 
 
 
-var buff = fs.readFileSync('./index.html', 'utf8');
+var buff = fs.readFileSync('index.html', 'utf8');
  
 
 var ear = buff.toString('utf-8',0,50);
 
 app.get('/', function(request, response) {
-    response.send('Henry');
+    response.send(ear);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
